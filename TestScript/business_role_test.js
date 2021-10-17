605737dc4131305da560b7ecf7511477b2be40be
thanhlamctk33 <87628745+thanhlamctk33@users.noreply.github.com>
2021-10-18 02:04:31 +0700

    Delete TestScript directory

--- TestScript/business_role_test.js
+++
@@ -1,9 +0,0 @@
-import Director from '../PageObject/business_role_page';
-import registerTest from './registration_test'
-
-test
-    ('Business Role', async t => {
-        registerTest
-        await Director.navigateDirectorForm();
-        await Director.fillInForm();
-});
--- TestScript/personal details_test.js
+++
@@ -1,6 +0,0 @@
-import PersonalDetails from '../POM/personal_details_page';
-
-test
-    ('Personal Details', async t => {
-        await PersonalDetails.editPersonalDetailsForm();
-});
