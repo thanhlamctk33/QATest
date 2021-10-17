d3c89f98dd0b56b6f0e7e7c89fc2a6015a67ecd8
thanhlamctk33 <87628745+thanhlamctk33@users.noreply.github.com>
2021-10-18 02:03:19 +0700

    Add files via upload

---
+++ Config/config.js
@@ -0,0 +1,5 @@
+export default {
+    url: 'https://feature-qa-automation.customer-frontend.staging.aspireapp.com/sg/',
+    otp:  '1 2 3 4',
+    fileUpload: '../Upload/Aspire+-+Board+Resolution_Client+Template+Form+v1'
+};
